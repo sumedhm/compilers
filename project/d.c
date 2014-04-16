25507498fff1ab451c2f7e8462b5cd6b665f420e
sumedhm <sumedhm18@gmail.com>
2014-04-16 19:38:05 +0530

    machinecode started

--- project/d.c
+++ project/d.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-
-y(int a, int b){
-	 z = a+b;
-	 return z;
-}
-
-main()
+int main()
 {
-	int i=0;
-	x=x+1;
-	if(i==1){
-		i=1;
-	}
-	y(2*3, a[2]);
-	
+	if(a=b && c == r || t<= 5+3*2 ) x=1;
 }
